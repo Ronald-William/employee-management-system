03facfda6b813351671e78ee86eeb03f8ae1d7eb
Ronald Joseph <ronaldjoseph439@gmail.com>
2026-05-11 15:47:10 +0530

    Added password validation

--- application/service/auth_service.py
+++ application/service/auth_service.py
@@ -7,15 +7,28 @@ from application.model.auth_model import TokenResponse
 from common.config.jwt_config import JwtConfig
 from common.middleware.logger import logger
 from common.error.exception import ConflictException, AuthException
+import string 
 
 password_hash = PasswordHash.recommended()
 
 class AuthService:
     def __init__(self, db: Session):
-        self.repository = UserRepository(db)
+        self.repository = UserRepository(db) 
+
+    def validate_password(self, password: str):
+        valid_len = True if len(password) >=8 else False
+        
+        has_upper = any(char.isupper() for char in password)
+        has_lower = any(char.islower() for char in password)
+        has_digit = any(char.isdigit() for char in password)
+        has_special = any(char in string.punctuation for char in password)
+        return valid_len and has_lower and has_upper and has_digit and has_special
     
     def register(self, email: str, password: str):
         existing = self.repository.get_by_email(email)
+        valid_password = self.validate_password(password)
+        if not valid_password:
+            return {"message" : "Password requirements not met", "requirements" : "Password must contain alteast 8 characters, one lowercase character, one uppercase character, one special_character and one digit"}
         if existing:
             logger.warning(f"Registration failed: Email {email} already exists.")
             raise ConflictException("email", email)
